04cdf27b524f97110aa5414aba9f05b91806af4c
Marax <marax@marax.cz>
2012-11-11 20:05:39 +0100

    sample VirtualButtons application from Vuforia

---
+++ obj/local/armeabi-v7a/objs-debug/VirtualButtons/Texture.o.d
@@ -0,0 +1,67 @@
+obj/local/armeabi-v7a/objs-debug/VirtualButtons/Texture.o: \
+ jni/Texture.cpp jni/Texture.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/jni.h \
+ jni/SampleUtils.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/stdio.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/stdint.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/android/log.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/string.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/malloc.h
+
+jni/Texture.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/jni.h:
+
+jni/SampleUtils.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/stdio.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/android/log.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/string.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/malloc.h:
---
+++ obj/local/armeabi-v7a/objs-debug/VirtualButtons/VirtualButtons.o.d
@@ -0,0 +1,158 @@
+obj/local/armeabi-v7a/objs-debug/VirtualButtons/VirtualButtons.o: \
+ jni/VirtualButtons.cpp \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/jni.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/android/log.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/stdio.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/stdint.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/string.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/malloc.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/assert.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/GLES2/gl2.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/GLES2/gl2platform.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/KHR/khrplatform.h \
+ C:/android-ndk/platforms/android-8/arch-arm/usr/include/GLES2/gl2ext.h \
+ jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/QCAR.h \
+ jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/System.h \
+ jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/UpdateCallback.h \
+ jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/CameraDevice.h \
+ jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/NonCopyable.h \
+ jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/VideoMode.h \
+ jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/CameraCalibration.h \
+ jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/Vectors.h \
+ jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/Renderer.h \
+ jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/Matrices.h \
+ jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/State.h \
+ jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/Frame.h \
+ jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/Area.h \
+ jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/Rectangle.h \
+ jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/VideoBackgroundConfig.h \
+ jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/Trackable.h \
+ jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/Tool.h \
+ jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/Tracker.h \
+ jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/TrackerManager.h \
+ jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/ImageTracker.h \
+ jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/ImageTarget.h \
+ jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/VirtualButton.h \
+ jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/DataSet.h \
+ jni/SampleUtils.h jni/Texture.h jni/CubeShaders.h jni/LineShaders.h \
+ jni/Teapot.h
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/jni.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/android/log.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/stdio.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/string.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/assert.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/GLES2/gl2.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/GLES2/gl2platform.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/KHR/khrplatform.h:
+
+C:/android-ndk/platforms/android-8/arch-arm/usr/include/GLES2/gl2ext.h:
+
+jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/QCAR.h:
+
+jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/System.h:
+
+jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/UpdateCallback.h:
+
+jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/CameraDevice.h:
+
+jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/NonCopyable.h:
+
+jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/VideoMode.h:
+
+jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/CameraCalibration.h:
+
+jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/Vectors.h:
+
+jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/Renderer.h:
+
+jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/Matrices.h:
+
+jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/State.h:
+
+jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/Frame.h:
+
+jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/Area.h:
+
+jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/Rectangle.h:
+
+jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/VideoBackgroundConfig.h:
+
+jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/Trackable.h:
+
+jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/Tool.h:
+
+jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/Tracker.h:
+
+jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/TrackerManager.h:
+
+jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/ImageTracker.h:
+
+jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/ImageTarget.h:
+
+jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/VirtualButton.h:
+
+jni/../../../../../../vuforia-sdk-android-1-5-9/build/include/QCAR/DataSet.h:
+
+jni/SampleUtils.h:
+
+jni/Texture.h:
+
+jni/CubeShaders.h:
+
+jni/LineShaders.h:
+
+jni/Teapot.h:
